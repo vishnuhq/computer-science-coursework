e616c0719df03761bed2ad2f833785709f495350
Vishnu Vardhan Putta <421pvv@gmail.com>
2026-03-14 11:44:09 +0530

    Add react router and navbar

--- ztm-coursework/complete-react-developer/capstone-project/src/main.jsx
+++ ztm-coursework/complete-react-developer/capstone-project/src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 import App from './App.jsx';
+
+import './index.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
