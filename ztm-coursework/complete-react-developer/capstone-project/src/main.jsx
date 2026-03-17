f4be1b418acec7f66745319498385fe98f97dad2
Vishnu Vardhan Putta <421pvv@gmail.com>
2026-03-17 13:08:04 +0530

    Add cart context and shop icon

--- ztm-coursework/complete-react-developer/capstone-project/src/main.jsx
+++ ztm-coursework/complete-react-developer/capstone-project/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import App from './App.jsx';
+import { CartProvider } from './contexts/cart.context';
+import { ProductsProvider } from './contexts/products.context';
 import { UserProvider } from './contexts/user.context';
 
 import './index.scss';
@@ -10,7 +12,11 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </StrictMode>
--- ztm-coursework/complete-react-developer/capstone-project/src/routes/navigation/navigation.component.jsx
+++ ztm-coursework/complete-react-developer/capstone-project/src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,12 @@
 import { useContext } from 'react';
 import { Link, Outlet } from 'react-router';
+
 import CrwnLogo from '../../assets/crown.svg?react';
+
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+
+import { CartContext } from '../../contexts/cart.context';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
@@ -8,6 +14,8 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <>
@@ -28,7 +36,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </>
